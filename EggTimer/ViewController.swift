a385c3885100caf9761d95808a7b27959628ffd5
Dan Lynch <dlynch_1@hotmail.com>
2022-07-04 17:15:02 -0500

    Finished Project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,21 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
     
-    @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle!
-        if (hardness == "Soft") {
-            print(softTime)
-        }else if (hardness == "Medium") {
-            print(mediumTime)
-        } else if (hardness == "Hard") {
-            print(hardTime)
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        progressBar.progress = 0.0
     }
     
+    let eggTimes = [
+        "Soft":3, "Medium":4, "Hard":7
+    ]
+    
+    var secondsRemaining = 0
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var eggStatus: UILabel!
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        progressBar.progress = 0.0
+        let hardness = sender.currentTitle!
+        eggStatus.text = hardness
+//        print(eggTimes[hardness]!)
+        secondsRemaining = eggTimes[hardness]!
+        let progbarPct = 1.0 / Float(secondsRemaining)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+                if self.secondsRemaining > 0 {
+                    print ("\(self.secondsRemaining) seconds")
+                    self.secondsRemaining -= 1
+                    self.progressBar.progress += progbarPct
+                } else {
+                    self.timer.invalidate()
+                    self.eggStatus.text = "Done!"
+                    self.playSound(theNote: "alarm_sound")
+                }
+            }
+    }
+    
+    func playSound(theNote: String) {
+        let url = Bundle.main.url(forResource: theNote, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
